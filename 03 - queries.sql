bc4397c487b1491448278c4d729f4806d12a466e
Liliana Antal <a.liliana23@gmail.com>
2024-01-26 22:18:33 +0200

    Add files via upload

--- 03 - queries.sql
+++ 03 - queries.sql
@@ -25,3 +25,35 @@ from Elevi
 join Clase
 	on Elevi.id_clasa = Clase.id_clasa
 where Clase.clasa = 'I_A';
+
+--Afisarea eleviilor unei clase date:
+go
+create or alter procedure usp_AfisareEleviClasa
+@numar_clasa varchar(10)
+as
+begin
+	select Clase.Clasa, Elevi.Nume, Elevi.Prenume 
+	from Elevi
+	join Clase
+		on Elevi.id_clasa = Clase.id_clasa
+	where Clase.clasa = @numar_clasa;
+end;
+go
+execute usp_AfisareEleviClasa @numar_clasa = 'V_B';
+
+--Afisare catalog (toti elevii din toate clasele cu notele aferente):
+select Clase.Clasa, Elevi.Nume, Elevi.Prenume, Materii.Nume_materie, 
+	   'Nota/Calificativ' = case 
+								when [Catalog].id_nota <=10 
+									then cast(Note.Nota as varchar(2))
+									else Note.Calificativ
+				            end
+from [Catalog]
+join Clase
+	on [Catalog].id_clasa = Clase.id_clasa
+join Elevi
+	on [Catalog].id_elev = Elevi.id_elev
+join Materii
+	on [Catalog].id_materie = Materii.id_materie
+join Note
+	on [Catalog].id_nota = Note.id_nota;
